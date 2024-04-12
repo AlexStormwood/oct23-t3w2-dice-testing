b255c90db7f6ba1b6e86f958b21f6aa0cbd44563
Alex H <alex.holder@bigfootds.com>
2024-04-13 09:45:24 +1000

    docs: add docs to dice roll functions

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,5 +1,13 @@
 
 
+/**
+ * Generate a random number between 1 and a given size.
+ * @date 4/13/2024 - 9:42:37 AM
+ * @author BigfootDS
+ *
+ * @param [diceSize=6] - Number representing the size of the dice you want to roll.
+ * @returns A random number between 1 and the given size.
+ */
 function rollDice(diceSize = 6){
 	let rollResult = Math.floor(Math.random() * diceSize) + 1;
 
@@ -7,6 +15,14 @@ function rollDice(diceSize = 6){
 }
 
 
+/**
+ * Generate a random number between 1 and a given size twice, and identifying the highest number.
+ * @date 4/13/2024 - 9:44:02 AM
+ * @author BigfootDS
+ *
+ * @param [diceSize=6]
+ * @returns A random number between 1 and a given size twice, and identifying the highest number.
+ */
 function rollDiceWithAdvantage(diceSize = 6){
 	let result = {
 		finalResult: null,
@@ -26,7 +42,14 @@ function rollDiceWithAdvantage(diceSize = 6){
 }
 
 
-
+/**
+ * Generate a random number between 1 and a given size twice, and identifying the smallest number.
+ * @date 4/13/2024 - 9:44:02 AM
+ * @author BigfootDS
+ *
+ * @param [diceSize=6]
+ * @returns A random number between 1 and a given size twice, and identifying the smallest number.
+ */
 function rollDiceWithDisadvantage(diceSize = 6){
 	let result = {
 		finalResult: null,

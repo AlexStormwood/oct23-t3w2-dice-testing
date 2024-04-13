d86083cef1ebdc84385ed6af74124c93769730f6
Alex H <alex.holder@bigfootds.com>
2024-04-13 10:45:13 +1000

    test: refactored test of dice varieties

--- tests/diceRoll.test.js
+++ tests/diceRoll.test.js
@@ -49,40 +49,82 @@ describe('As a dungeon master...', () => {
 	
 	describe('...I want to roll a variety of dice sizes, such as ...', () => { 
 	
-		test('D6', () => { 
-			let result = rollDice();
 
-			expect(result).toBeGreaterThan(0);
-			expect(result).toBeLessThanOrEqual(6);
+		let diceVarieties = [
+			{
+				diceSize: 4,
+				diceName: "D4"
+			},
+			{
+				diceSize: 6,
+				diceName: "D6"
+			},
+			{
+				diceSize: 8,
+				diceName: "D8"
+			},
+			{
+				diceSize: 10,
+				diceName: "D10"
+			},
+			{
+				diceSize: 12,
+				diceName: "D12"
+			},
+			{
+				diceSize: 20,
+				diceName: "D20"
+			},
+			{
+				diceSize: 100,
+				diceName: "D100"
+			},
+		];
+
+		test.each(diceVarieties)("$diceName", (diceObject) => {
+			let diceResult = rollDice(diceObject.diceSize);
+			expect(diceResult).toBeGreaterThan(0);
+			expect(diceResult).toBeLessThanOrEqual(diceObject.diceSize);
 		});
 
-		test('D8', () => { 
-			let result = rollDice(8);
+		
 
-			expect(result).toBeGreaterThan(0);
-			expect(result).toBeLessThanOrEqual(8);
-		});
 
-		test('D10', () => { 
-			let result = rollDice(10);
 
-			expect(result).toBeGreaterThan(0);
-			expect(result).toBeLessThanOrEqual(10);
-		});
+		// test('D6', () => { 
+		// 	let result = rollDice();
 
-		test('D12', () => { 
-			let result = rollDice(12);
+		// 	expect(result).toBeGreaterThan(0);
+		// 	expect(result).toBeLessThanOrEqual(6);
+		// });
 
-			expect(result).toBeGreaterThan(0);
-			expect(result).toBeLessThanOrEqual(12);
-		});
+		// test('D8', () => { 
+		// 	let result = rollDice(8);
 
-		test('D20', () => { 
-			let result = rollDice(20);
+		// 	expect(result).toBeGreaterThan(0);
+		// 	expect(result).toBeLessThanOrEqual(8);
+		// });
 
-			expect(result).toBeGreaterThan(0);
-			expect(result).toBeLessThanOrEqual(20);
-		});
+		// test('D10', () => { 
+		// 	let result = rollDice(10);
+
+		// 	expect(result).toBeGreaterThan(0);
+		// 	expect(result).toBeLessThanOrEqual(10);
+		// });
+
+		// test('D12', () => { 
+		// 	let result = rollDice(12);
+
+		// 	expect(result).toBeGreaterThan(0);
+		// 	expect(result).toBeLessThanOrEqual(12);
+		// });
+
+		// test('D20', () => { 
+		// 	let result = rollDice(20);
+
+		// 	expect(result).toBeGreaterThan(0);
+		// 	expect(result).toBeLessThanOrEqual(20);
+		// });
 	})
 
 });
@@ -91,5 +133,5 @@ describe('As a dungeon master...', () => {
 
 test('Top level test is here', () => { 
 	console.log("Hello world");
-			
+
 });
